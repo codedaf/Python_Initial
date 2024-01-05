c6d3e66b7eae5334e0383f8f5516fa94bccedfc1
code_daf <codedaf1@gmail.com>
2024-01-05 14:26:49 +1030

    first commit

---
+++ 1_variables.py
@@ -0,0 +1,29 @@
+"""
+Variables
+In Python, variables are identifiers that store and represent data in memory. 
+"""
+user='user001'
+password='123'
+first_name = 'Diego'
+last_name = 'Fernandez'
+country = 'Argentina'
+city = 'Avellaneda'
+age = 39
+
+print("User login: ", user)
+print("Name:", first_name , " - Last Name:",last_name)
+print("Country:", country , " - City:",city)
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
