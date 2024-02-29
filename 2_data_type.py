3619f86a3ccaf9dc93d55014d7733ace323b7de5
codedaf <155696815+codedaf@users.noreply.github.com>
2024-02-29 11:58:36 +1030

    Update 2_data_type.py

--- 2_data_type.py
+++ 2_data_type.py
@@ -1,1 +1,41 @@
+# Integer (int)
+integer_number = 10
+print("Integer:", integer_number)
 
+# Float (float)
+float_number = 3.14
+print("Float:", float_number)
+
+# String (str)
+string_value = "Hello, world!"
+print("String:", string_value)
+
+# Boolean (bool)
+true_value = True
+false_value = False
+print("Boolean (True):", true_value)
+print("Boolean (False):", false_value)
+
+# List
+list_data = [1, 2, 3, 4, 5]
+print("List:", list_data)
+
+# Tuple
+tuple_data = (1, 2, 3, 4, 5)
+print("Tuple:", tuple_data)
+
+# Dictionary
+dictionary_data = {'key1': 'value1', 'key2': 'value2'}
+print("Dictionary:", dictionary_data)
+
+# Set
+set_data = {1, 2, 3, 4, 5}
+print("Set:", set_data)
+
+# NoneType (None)
+none_value = None
+print("None:", none_value)
+
+# Bytes
+bytes_data = b'Hello'
+print("Bytes:", bytes_data)
