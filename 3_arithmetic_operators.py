7524018d88fd30d317e425a88e11df536950b0c7
codedaf <155696815+codedaf@users.noreply.github.com>
2024-02-29 12:02:41 +1030

    Create 3_arithmetic_operators.py

---
+++ 3_arithmetic_operators.py
@@ -0,0 +1,31 @@
+# Addition (+)
+result_addition = 5 + 3
+print("Addition:", result_addition)
+
+# Subtraction (-)
+result_subtraction = 7 - 2
+print("Subtraction:", result_subtraction)
+
+# Multiplication (*)
+result_multiplication = 4 * 6
+print("Multiplication:", result_multiplication)
+
+# Division (/)
+result_division = 10 / 2
+print("Division:", result_division)
+
+# Integer Division (//)
+result_integer_division = 10 // 3
+print("Integer Division:", result_integer_division)
+
+# Modulus (%)
+result_modulus = 10 % 3
+print("Modulus:", result_modulus)
+
+# Exponentiation (**)
+result_exponentiation = 2 ** 3
+print("Exponentiation:", result_exponentiation)
+
+# Negation (-)
+result_negation = -5
+print("Negation:", result_negation)
